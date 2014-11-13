d6c474ff336f255516f14d55aab9d8d92a8563c6
adebisi <profadexbj@gmail.com>
2014-11-13 12:58:41 -0500

    Update partner.php

--- partner.php
+++ partner.php
@@ -19,7 +19,165 @@
 		
             <!-- Include side bar -->
             <?php include ('includes/sidebar.php') ?>
-			
+<?php
+
+if(ISSET($_POST['invita'])){
+function get_rand_id($length)
+{
+  if($length>0) 
+  { 
+  $rand_id="";
+   for($i=1; $i<=$length; $i++)
+   {
+   mt_srand((double)microtime() * 1000000);
+   $num = mt_rand(1,36);
+   $rand_id .= assign_rand_value($num);
+   }
+  }
+return $rand_id;
+} 	
+	
+function assign_rand_value($num)
+{
+// accepts 1 - 36
+  switch($num)
+  {
+    case "1":
+     $rand_value = "a";
+    break;
+    case "2":
+     $rand_value = "b";
+    break;
+    case "3":
+     $rand_value = "c";
+    break;
+    case "4":
+     $rand_value = "d";
+    break;
+    case "5":
+     $rand_value = "e";
+    break;
+    case "6":
+     $rand_value = "f";
+    break;
+    case "7":
+     $rand_value = "g";
+    break;
+    case "8":
+     $rand_value = "h";
+    break;
+    case "9":
+     $rand_value = "i";
+    break;
+    case "10":
+     $rand_value = "j";
+    break;
+    case "11":
+     $rand_value = "k";
+    break;
+    case "12":
+     $rand_value = "l";
+    break;
+    case "13":
+     $rand_value = "m";
+    break;
+    case "14":
+     $rand_value = "n";
+    break;
+    case "15":
+     $rand_value = "o";
+    break;
+    case "16":
+     $rand_value = "p";
+    break;
+    case "17":
+     $rand_value = "q";
+    break;
+    case "18":
+     $rand_value = "r";
+    break;
+    case "19":
+     $rand_value = "s";
+    break;
+    case "20":
+     $rand_value = "t";
+    break;
+    case "21":
+     $rand_value = "u";
+    break;
+    case "22":
+     $rand_value = "v";
+    break;
+    case "23":
+     $rand_value = "w";
+    break;
+    case "24":
+     $rand_value = "x";
+    break;
+    case "25":
+     $rand_value = "y";
+    break;
+    case "26":
+     $rand_value = "z";
+    break;
+    case "27":
+     $rand_value = "0";
+    break;
+    case "28":
+     $rand_value = "1";
+    break;
+    case "29":
+     $rand_value = "2";
+    break;
+    case "30":
+     $rand_value = "3";
+    break;
+    case "31":
+     $rand_value = "4";
+    break;
+    case "32":
+     $rand_value = "5";
+    break;
+    case "33":
+     $rand_value = "6";
+    break;
+    case "34":
+     $rand_value = "7";
+    break;
+    case "35":
+     $rand_value = "8";
+    break;
+    case "36":
+     $rand_value = "9";
+    break;
+  }
+return $rand_value;
+}
+?>
+<?php
+
+$email =$_POST['invita'];
+
+$mess=get_rand_id(6);
+$to=$email; 
+$subject="Family Budget Planner"; 
+$header="From:budgetplanner@mysite.com"; 
+$message="Dear User,\n Your friend invite you to use Budget Planner Application \n Use this temporary information to login in \n
+\n Temporary username: $email\n Temporary password: $mess \n  click below link to go to the website \n http://localhost:1337/hci535project \n"; 
+
+if(mail($to,$subject,$message,$header)) 
+{ 
+$_POST['invitationsent']="mail sent successfully";
+
+
+} 
+else 
+{ 
+$_POST['invitationsent']= "mail not sent..error,  try again"; 
+
+}
+}
+?>			
             <aside class="right-side">
 				<!-- Content Header (Page header) -->
 				<section class="content-header">
@@ -34,12 +192,13 @@
 					<div class="box-header">
 						<h3 class="box-title">Add Partners</h3>
 					</div>
-					<form method="POST" action="./partner.php" >
+                    <? if(ISSET($_POST['invitationsent'])){echo $_POST['invitationsent']; }?>
+					<form method="POST" action="partner.php" >
 						<div class="box-body">
 							<label>Email:</label>
 							<div class="input-group">
 								<span class="input-group-addon">@</span>
-								<input class="form-control" type="text" placeholder="Email">
+								<input class="form-control" type="text" placeholder="Email" name="invita">
 							</div>
 							<br/>
 							
